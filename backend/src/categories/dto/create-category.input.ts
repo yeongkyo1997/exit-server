a4f14cc4ef0eed333922b638fe0492922f2fbc50
yeongkyo1997 <yeongkyo1997@gmail.com>
2022-08-31 15:54:29 +0900

    feature: category CRUD

--- backend/src/categories/dto/create-category.input.ts
+++ backend/src/categories/dto/create-category.input.ts
@@ -3,5 +3,5 @@ import { InputType, Field } from "@nestjs/graphql";
 @InputType()
 export class CreateCategoryInput {
   @Field(() => String, { description: "Example field (placeholder)" })
-  id: string;
+  name: string;
 }
