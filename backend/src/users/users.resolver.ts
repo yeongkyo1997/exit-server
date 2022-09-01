ed58885a3e295d081e935cff32bef9d583f29834
yeongkyo1997 <yeongkyo1997@gmail.com>
2022-09-01 12:58:46 +0900

    feature: user CRUD

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -3,33 +3,44 @@ import { UsersService } from "./users.service";
 import { User } from "./entities/user.entity";
 import { CreateUserInput } from "./dto/create-user.input";
 import { UpdateUserInput } from "./dto/update-user.input";
+import * as bcrypt from "bcrypt";
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Mutation(() => User)
-  createUser(@Args("createUserInput") createUserInput: CreateUserInput) {
-    return this.usersService.create(createUserInput);
+  async createUser(@Args("createUserInput") createUserInput: CreateUserInput) {
+    const { password, ...rest } = createUserInput;
+
+    const hashedPassword = await bcrypt.hash(password, 10.2);
+
+    return this.usersService.create({
+      createUserInput,
+      password: hashedPassword,
+    });
   }
 
-  @Query(() => [User], { name: "users" })
-  findAll() {
+  @Query(() => [User])
+  fetchUsers() {
     return this.usersService.findAll();
   }
 
-  @Query(() => User, { name: "user" })
-  findOne(@Args("id", { type: () => String }) id: string) {
-    return this.usersService.findOne(id);
+  @Query(() => User)
+  fetchUser(@Args("userId", { type: () => String }) userId: string) {
+    return this.usersService.findOne({ userId });
   }
 
   @Mutation(() => User)
-  updateUser(@Args("updateUserInput") updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
+  removeUser(@Args("userId", { type: () => String }) userId: string) {
+    return this.usersService.remove({ userId });
   }
 
   @Mutation(() => User)
-  removeUser(@Args("id", { type: () => String }) id: string) {
-    return this.usersService.remove(id);
+  updateUser(
+    @Args("userId") userId: string,
+    @Args("updateUserInput") updateUserInput: UpdateUserInput
+  ) {
+    return this.usersService.update({ userId, updateUserInput });
   }
 }
