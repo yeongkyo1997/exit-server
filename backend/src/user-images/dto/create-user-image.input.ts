9ced0542b97a3ee7d58a5194bca377b4d99c98bb
yeongkyo1997 <yeongkyo1997@gmail.com>
2022-09-01 13:08:55 +0900

    bugfix: userImages

--- backend/src/user-images/dto/create-user-image.input.ts
+++ backend/src/user-images/dto/create-user-image.input.ts
@@ -2,6 +2,6 @@ import { InputType, Field } from "@nestjs/graphql";
 
 @InputType()
 export class CreateUserImageInput {
-  @Field(() => String, { description: "Example field (placeholder)" })
-  id: string;
+  @Field(() => String)
+  url: string;
 }
