2eb77271634cd98c882f8bb1a3cb82be149f9554
yeongkyo1997 <yeongkyo1997@gmail.com>
2022-09-01 11:00:44 +0900

    feature: keyword CRUD

--- backend/src/keywords/keywords.service.ts
+++ backend/src/keywords/keywords.service.ts
@@ -1,26 +1,60 @@
 import { Injectable } from "@nestjs/common";
-import { CreateKeywordInput } from "./dto/create-keyword.input";
-import { UpdateKeywordInput } from "./dto/update-keyword.input";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Keyword } from "./entities/keyword.entity";
 
 @Injectable()
 export class KeywordsService {
-  create(createKeywordInput: CreateKeywordInput) {
-    return "This action adds a new keyword";
+  constructor(
+    @InjectRepository(Keyword)
+    private keywordsRepository: Repository<Keyword>
+  ) {}
+
+  create(createKeywordInput) {
+    const checkKeyword = this.keywordsRepository.findOne({
+      where: { name: createKeywordInput.name },
+    });
+
+    // 같은 이름의 키워드가 존재한다면 에러를 던진다.
+    if (checkKeyword) {
+      throw new Error("이미 존재하는 키워드입니다.");
+    }
+
+    return this.keywordsRepository.save(createKeywordInput);
   }
 
   findAll() {
-    return `This action returns all keywords`;
+    return this.keywordsRepository.find();
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} keyword`;
+    return this.keywordsRepository.findOne({ where: { id } });
   }
 
-  update(id: string, updateKeywordInput: UpdateKeywordInput) {
-    return `This action updates a #${id} keyword`;
+  async update(id: string, updateKeywordInput) {
+    const keyword = await this.keywordsRepository.findOne({ where: { id } });
+    // 키워드가 존재하지 않는다면 에러를 던진다.
+    if (!keyword) {
+      throw new Error("존재하지 않는 키워드입니다.");
+    }
+    const checkKeyword = await this.keywordsRepository.findOne({
+      where: { name: updateKeywordInput.name },
+    });
+
+    // 같은 이름의 키워드가 존재한다면 에러를 던진다.
+    if (checkKeyword) {
+      throw new Error("이미 존재하는 키워드입니다.");
+    }
+
+    return this.keywordsRepository.save({ ...keyword, ...updateKeywordInput });
   }
 
   remove(id: string) {
-    return `This action removes a #${id} keyword`;
+    const keyword = this.keywordsRepository.findOne({ where: { id } });
+    // 키워드가 존재하지 않는다면 에러를 던진다.
+    if (!keyword) {
+      throw new Error("존재하지 않는 키워드입니다.");
+    }
+    return this.keywordsRepository.softDelete({ id });
   }
 }
