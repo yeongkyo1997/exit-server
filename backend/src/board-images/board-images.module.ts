8d74860900a31a7347382bbbeaeac9207c58c9c4
yeongkyo <yeongkyo1997@gmail.com>
2022-09-04 23:29:48 +0900

    edit: board-images.module.ts

--- backend/src/board-images/board-images.module.ts
+++ backend/src/board-images/board-images.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { BoardImagesService } from "./board-images.service";
 import { BoardImagesResolver } from "./board-images.resolver";
+import { BoardImage } from "./entities/board-image.entity";
 
 @Module({
+  imports: [BoardImage],
   providers: [BoardImagesResolver, BoardImagesService],
 })
 export class BoardImagesModule {}
