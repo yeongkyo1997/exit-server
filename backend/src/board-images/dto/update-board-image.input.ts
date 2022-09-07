9bbe0725d5990954a5e5acb0cb5361d757d1796f
bluephone715 <bluephone715@gmail.com>
2022-09-07 10:57:25 +0900

    bugfix user

--- backend/src/board-images/dto/update-board-image.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateBoardImageInput } from "./create-board-image.input";
-import { InputType, Field, PartialType } from "@nestjs/graphql";
-
-@InputType()
-export class UpdateBoardImageInput extends PartialType(CreateBoardImageInput) {
-  @Field(() => String)
-  id: string;
-}
